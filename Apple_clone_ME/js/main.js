07851632cc9cf3cc95e10013829c29a141f2b4ec
hwanyamyu@gmail.com <hwanyamyu@gmail.com>
2021-09-14 14:59:34 +0900

    20210914

--- Apple_clone_ME/js/main.js
+++ Apple_clone_ME/js/main.js
@@ -1,6 +1,11 @@
 // (function() {})(); 와 같은 형태
 // 이렇게 하는 이유? : 전역변수 사용을 피하려고 / 이름간 충돌이 발생할 수 있음 / 자바스크립트 자체에서 전역변수 사용은 바람직하지 않음
 (() => {
+
+    let yOffset = 0;  //window.pageYOffset 대신 쓸 변수
+    let prevScrollHeight = 0; //현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+    let currentScene = 0; //현재 활성화된(눈 앞에 보고 있는) 씬(scroll-section)
+
     const sceneInfo = [
         {
             // 0
@@ -48,8 +53,33 @@
         }
     }
     
+    function scrollLoop(){ //애니메이션 활성화시킬 씬이 몇번째 씬인지 알아내는
+        // console.log(yOffset);
+        prevScrollHeight = 0; //scrollHeight의 값 누적을 막기 위해
+        for(let i = 0; i < currentScene; i++) {
+            // prevScrollHeight = prevScrollHeight + sceneInfo[i].scrollHeight;
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+        if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
+            currentScene++;
+        }
+        if(yOffset < prevScrollHeight) {
+            if(currentScene === 0) return //스크롤시 브라우저 바운스 효과로 인해 마이너스 되는 것을 방지(모바일)
+            currentScene--;
+        }
+
+        console.log(currentScene)
+    }
+
+
     window.addEventListener( 'resize', setLayout )
 
-    setLayout();
+    window.addEventListener( 'scroll', ()=> { //스크롤은 복잡하게 동작하기 때문에 여러 함수 사용
+        yOffset = window.pageYOffset;
+        scrollLoop();
 
+    });
+
+    setLayout();
+    
 })();
